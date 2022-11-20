202522c8a14e4b2295ad6fbf25e854a2e9e45364
EzequielK-source <programacionezequielk@gmail.com>
2022-11-20 20:34:04 -0300

    Implement postJob and findJob - Call postJob in jobs/ route with POST Method - Call findJob in jobs/:id route with GET Method

--- backend/src/core/jobs/routes.js
+++ backend/src/core/jobs/routes.js
@@ -1,12 +1,17 @@
 const jobsRouter = require('express').Router()
-
+const {postJob, findJob} = require('./services/index')
 
 jobsRouter.route('/')
-  .post((req,res)=>{
+  .post(async(req,res)=>{
+    const job = await postJob(req.body)
     return res.status(201).json({
       status: 'Job posted',
-      job: req.body
+      job
     })
   })
-
+jobsRouter.route('/:id')
+  .get(async(req,res)=>{
+    const findedJob = await findJob(req.params.id)
+    return res.status(200).json({job: findedJob})
+  })
 module.exports = jobsRouter
