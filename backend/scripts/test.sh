85594d2ee750d8591423d36844e248d230c5f041
EzequielK-source <programacionezequielk@gmail.com>
2022-11-20 20:18:50 -0300

    Add database ENV

--- backend/scripts/test.sh
+++ backend/scripts/test.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 export NODE_PATH='.'
+
+export DATABASE_NAME='jobs_portal_test'
+export DATABASE_USER='root'
+export DATABASE_PASSWORD=''
+export DATABASE_HOST='localhost'
+
+
 jest "$1" "$2"
