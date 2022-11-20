88c65e11bf329c35e4fbfa73a6d8380b4110f758
EzequielK-source <programacionezequielk@gmail.com>
2022-11-20 19:35:04 -0300

    Add jobs route and link with router -Add morgan middleware -Add cors middleware -Add express.json middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,16 @@
-const Express = require('express')
-const app = Express()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const app = express()
+app.use(express.json())
+app.use(morgan('dev'))
+app.use(cors())
+
+
+
+//Routes
+const jobsRoutes = require('src/core/jobs/routes.js')
+app.use('/jobs', jobsRoutes)
+
 
 module.exports = app
