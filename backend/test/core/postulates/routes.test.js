9355f8b937ef7965e0b556456d3ee4f020592267
EzequielK-source <programacionezequielk@gmail.com>
2022-11-21 19:45:53 -0300

    Add send to post request

--- backend/test/core/postulates/routes.test.js
+++ backend/test/core/postulates/routes.test.js
@@ -1,13 +1,20 @@
 const request = require('supertest')
 const app = require('src/app')
 
-
 describe('Postulates routes test', () => {
   describe('route /:jobId param test', () => {
     let jobId = 'nonCreatedJob'
+    let postulateSend = {
+      user_id: 'fakeUserID',
+      email: 'example@email.com',
+      job_id: jobId,
+      message: 'Postulate message',
+      cv: 'path/to/cv'
+    }
     test('POST (Apply job)', done => {
       request(app)
         .post(`/postulates/${jobId}`)
+        .send(postulateSend)
         .expect('Content-type', /json/)
         .then(({ statusCode, body }) => {
           expect(statusCode).toBe(201)
@@ -26,6 +33,9 @@ describe('Postulates routes test', () => {
           const { postulates } = body
 
           expect(postulates).toBeInstanceOf(Array)
+          expect(postulates.length > 1).toBe(true)
+
+          expect(postulates[0]).toMatchObject(postulateSend)
           done()
         })
     })
