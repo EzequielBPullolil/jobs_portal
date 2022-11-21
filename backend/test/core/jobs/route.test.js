112694869c746ed9302038057abf60cf345a3cbb
EzequielK-source <programacionezequielk@gmail.com>
2022-11-20 21:36:17 -0300

    Adapt jobs routes test for new Jobs model

--- backend/test/core/jobs/route.test.js
+++ backend/test/core/jobs/route.test.js
@@ -2,13 +2,16 @@ const {validate:validateUUID} = require('uuid')
 const app = require('src/app')
 const request = require('supertest')
 describe('Jobs routes test', () => {
-  const job_suject_params = {
-    title: 'Job'
+  const jobSujectParms = {
+    title: 'Job',
+    email: 'test@mail.com',
+    description: 'a test description for job',
+    miinimal_experience: 0
   }
   test('POST (post job)', done => {
     request(app)
       .post('/jobs')
-      .send(job_suject_params)
+      .send(jobSujectParms)
       .expect('Content-Type', /json/)
       .then(response=>{
         expect(response.statusCode).toBe(201)
@@ -17,18 +20,18 @@ describe('Jobs routes test', () => {
         const {status, job} = response.body
 
         expect(status).toBe('Job posted')
-        expect(job).toHaveProperty('title', job_suject_params.title)
+        expect(job).toHaveProperty('title', jobSujectParms.title)
         expect(job).toHaveProperty('id')
         expect(validateUUID(job.id)).toBe(true)
 
-        job_suject_params.id = job.id
+        jobSujectParms.id = job.id
         done() //
       })
   })
   describe('jobs/:id route tests', () => {
     test('GET (Detail post)', done => {
       request(app)
-        .get(`/jobs/${job_suject_params.id}`)
+        .get(`/jobs/${jobSujectParms.id}`)
         .expect('Content-type', /json/)
         .then(response=>{
           expect(response.statusCode).toBe(200)
@@ -36,7 +39,7 @@ describe('Jobs routes test', () => {
 
           const { title } = response.body.job
 
-          expect(title).toBe(job_suject_params.title)
+          expect(title).toBe(jobSujectParms.title)
           done()
         })
     })
