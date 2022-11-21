ac64628a9cb515fa4dad9173c8bd2d4d9886f43d
EzequielK-source <programacionezequielk@gmail.com>
2022-11-21 18:55:04 -0300

    Add test to postulates route test -Add test group for postulates/:jobId route -Move POST (apply job) test into test group -Add GET request test for postulates/:jobId

--- backend/test/core/postulates/routes.test.js
+++ backend/test/core/postulates/routes.test.js
@@ -3,15 +3,31 @@ const app = require('src/app')
 
 
 describe('Postulates routes test', () => {
-  let jobId = 'nonCreatedJob'
-  test('POST (Apply job)', done => {
-    request(app)
-      .post(`/postulates/${jobId}`)
-      .expect('Content-type', /json/)
-      .then(({statusCode, body})=>{
-        expect(statusCode).toBe(201)
-        expect(body).toHaveProperty('status', 'successful application')
-        done()
-      })
+  describe('route /:jobId param test', () => {
+    let jobId = 'nonCreatedJob'
+    test('POST (Apply job)', done => {
+      request(app)
+        .post(`/postulates/${jobId}`)
+        .expect('Content-type', /json/)
+        .then(({ statusCode, body }) => {
+          expect(statusCode).toBe(201)
+          expect(body).toHaveProperty('status', 'successful application')
+          done()
+        })
+    })
+
+    test('GET (Show ', done => {
+      request(app)
+        .get(`/postulates/${jobId}`)
+        .expect('Content-type', /json/)
+        .then(({ statusCode, body }) => {
+          expect(statusCode).toBe(200)
+          expect(body).toHaveProperty('postulates')
+          const { postulates } = body
+
+          expect(postulates).toBeInstanceOf(Array)
+          done()
+        })
+    })
   })
 })
