ca8c6895cc1ecae95d879a020f6ba36fad48d435
EzequielK-source <programacionezequielk@gmail.com>
2022-11-21 19:59:32 -0300

    Now global jest setup call MakeRelations

--- backend/test/setup.js
+++ backend/test/setup.js
@@ -1,4 +1,6 @@
 const SyncModels = require('src/services/sequelize/functions/sync_models')
+const MakeRelations = require('src/services/sequelize/functions/make_relations')
 module.exports = async()=>{
   await SyncModels(true)
+  await MakeRelations()
 }
