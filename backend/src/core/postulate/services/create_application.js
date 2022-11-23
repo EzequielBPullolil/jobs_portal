755e4a532093368952df2658c4197fe15de54f49
EzequielK-source <programacionezequielk@gmail.com>
2022-11-23 17:30:06 -0300

    Remove temporarily user_id and add cv from create call

--- backend/src/core/postulate/services/create_application.js
+++ backend/src/core/postulate/services/create_application.js
@@ -1,13 +1,12 @@
 const { PostulateModel } = require('src/services/sequelize/index')
 const { v4: uuidv4 } = require('uuid')
-module.exports = async ({ user_id, email,  job_id, message}) => {
+module.exports = async ({ user_id, email,  jobId, message, cv}) => {
   /**
    * Persit postulate and return they id, email, message and cv
    */
 
   const id = uuidv4()
-  await PostulateModel.create({id, user_id, email, job_id, message})
-
+  await PostulateModel.create({id, email, fk_jobs_id: jobId, message, cv})
   return {
     id, email, message
   }
