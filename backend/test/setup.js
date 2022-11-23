cea475c20539786574baa079ff8cde45dc2e5b61
EzequielK-source <programacionezequielk@gmail.com>
2022-11-23 17:34:04 -0300

    Add try catch to jest global setup

--- backend/test/setup.js
+++ backend/test/setup.js
@@ -1,6 +1,10 @@
 const SyncModels = require('src/services/sequelize/functions/sync_models')
 const MakeRelations = require('src/services/sequelize/functions/make_relations')
 module.exports = async()=>{
-  await SyncModels(true)
-  await MakeRelations()
+  try{
+    await SyncModels(true)
+    await MakeRelations()
+  }catch(err){
+    console.error(err)
+  }
 }
