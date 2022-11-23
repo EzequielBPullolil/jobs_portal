135cbf9954f9cfd6611ea1ec975333d0a8af0729
EzequielK-source <programacionezequielk@gmail.com>
2022-11-23 17:35:02 -0300

    Create ObtainPostulates services

---
+++ backend/src/core/postulate/services/obtain_postulates.js
@@ -0,0 +1,14 @@
+const { PostulateModel } = require('src/services/sequelize/index')
+module.exports = async(jobId) => {
+  /**
+   * Find all postulates from job and return array of Postulates
+   *
+   * @param jobId Job.id
+   * @return Array of Postulates
+   */
+
+
+  const postulatesFindend = await PostulateModel.findAll({where: {fk_jobs_id: jobId}})
+
+  return postulatesFindend
+}
