215ef45469eb865c727f7918d744b3971eec95e4
EzequielK-source <programacionezequielk@gmail.com>
2022-11-23 17:36:38 -0300

    Add ObtainPostuolates services to get method -Add try catch vor every route

--- backend/src/core/postulate/routes.js
+++ backend/src/core/postulate/routes.js
@@ -1,17 +1,33 @@
 const postulatesRouter = require('express').Router()
-const {CreateApplication} = require('./services/index')
+const {CreateApplication, ObtainPostulates} = require('./services/index')
 postulatesRouter.route('/:jobId')
   .post(async (req,res)=>{
-    const apply = await CreateApplication(req.body)
-    return res.status(201).json({
-      status: 'successful application',
-      apply
-    })
+    try {
+      const applicationParams = {
+        jobId: req.params.jobId,
+        ...req.body
+      }
+      console.log(applicationParams)
+      const apply = await CreateApplication(applicationParams)
+      return res.status(201).json({
+        status: 'successful application',
+        apply
+      })
+    }catch(err){
+      console.error(err)
+      return res.status(400).json({error: err.name, message: err.message})
+    }
   })
-  .get((req,res)=>{
-    return res.status(200).json({
-      postulates: []
-    })
+  .get(async(req,res)=>{
+    try {
+      const postulates = await ObtainPostulates(req.params.jobId)
+      return res.status(200).json({
+        postulates
+      })
+    }catch(err){
+      console.error(err)
+      return res.status(400).json({error:err.name, message: err.message})
+    }
   })
 
 
