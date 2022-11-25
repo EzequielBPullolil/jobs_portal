5af6f13fe49f97e46318e360a03d587aceb76803
EzequielK-source <programacionezequielk@gmail.com>
2022-11-25 16:31:17 -0300

    Create get_postulates page -Create component PostulatePreview

---
+++ frontend/components/postulate/PostulatePreview.vue
@@ -0,0 +1,20 @@
+<template lang="">
+  <div>
+    <h2>{{ postulate.email }}</h2>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    postulate: {
+      type: {},
+      default () {
+        return { email: '', message: '' }
+      }
+    }
+  }
+}
+</script>
+<style lang="">
+
+</style>
---
+++ frontend/pages/postulates/get_postulates/_id.vue
@@ -0,0 +1,21 @@
+<template lang="">
+  <div>
+    <h1>Job postulates</h1>
+    <PostulatePreview v-for="postulate in postulates" :key="postulate.id" :postulate="postulate" />
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      postulates: []
+    }
+  },
+  async created () {
+    await this.$axios.get(`/api/postulates/${this.$route.params.id}`)
+      .then(response => response.data)
+      .then(data => data.postulates)
+      .then((postulates) => { this.postulates = postulates })
+  }
+}
+</script>
