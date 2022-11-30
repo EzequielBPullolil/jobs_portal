6eb28aaa9fd46f2758fac1c4d05883f47bce3b54
EzequielK-source <programacionezequielk@gmail.com>
2022-11-30 09:38:30 -0300

    Modify UploadCv middleware for storage cv_path in request

--- backend/src/core/postulate/middlewares/upload_cv.js
+++ backend/src/core/postulate/middlewares/upload_cv.js
@@ -1,7 +1,7 @@
 const UploadCv = require('src/services/upload_cv')
 module.exports = (req, res, next) => {
   if (!req.files || Object.keys(req.files).length === 0) return res.status(400).json({ error: 'Missing cv file' })
-  UploadCv(req.files.cv, req.params.jobId)
-
+  const cvPath = UploadCv(req.files.cv, req.params.jobId)
+  req.cv_path = cvPath
   next()
 }
