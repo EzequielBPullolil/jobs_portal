98f679635b2c7050681eaedff36a004cf458c468
EzequielK-source <programacionezequielk@gmail.com>
2022-11-30 19:23:15 -0300

    Implement SingUser services in root POST request

--- backend/src/core/users/routes.js
+++ backend/src/core/users/routes.js
@@ -1,10 +1,17 @@
+const {SingUser} = require('./services/index')
 const userRouter = require('express').Router()
-
 userRouter.route('/')
   .post(async(req,res)=>{
-    return res.status(201).json({
-      status: 'user created'
-    })
+    try{
+      const user = await SingUser(req.body)
+      return res.status(201).json({
+        status: 'user created',
+        user
+      })
+    }catch(err){
+      console.error(err)
+      return res.status(400).json(err)
+    }
   })
 
 module.exports = userRouter
