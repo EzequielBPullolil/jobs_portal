008592d604e574b50befc307b17d6893b721885a
EzequielK-source <programacionezequielk@gmail.com>
2022-11-30 18:53:56 -0300

    Now after  jest setup clear console

--- backend/test/setup.js
+++ backend/test/setup.js
@@ -2,8 +2,13 @@ const SyncModels = require('src/services/sequelize/functions/sync_models')
 const MakeRelations = require('src/services/sequelize/functions/make_relations')
 module.exports = async()=>{
   try{
-    await SyncModels(true)
-    await MakeRelations()
+    await Promise.all([
+      SyncModels(true),
+      MakeRelations()
+    ])
+      .then(()=>{
+        console.clear()
+      })
   }catch(err){
     console.error(err)
   }
