53ba27b676806b5477075812dd43c45ee6231964
EzequielK-source <programacionezequielk@gmail.com>
2022-11-30 09:26:35 -0300

    Add validation for email field

--- backend/src/core/postulate/services/create_application.js
+++ backend/src/core/postulate/services/create_application.js
@@ -4,7 +4,7 @@ module.exports = async ({ user_id, email,  jobId, message, cv_path}) => {
   /**
    * Persit postulate and return they id, email, message and cv
    */
-
+  if(!email || email === '' ) throw new Error('Missing email')
   const id = uuidv4()
   await PostulateModel.create({id, email, fk_jobs_id: jobId, message, cv_path})
   return {
