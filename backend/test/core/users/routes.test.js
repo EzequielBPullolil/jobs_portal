08eb7f631dcd63ae302743ca657acd7024be26ea
EzequielK-source <programacionezequielk@gmail.com>
2022-11-30 19:01:37 -0300

    Agroup /users POST in test suite

--- backend/test/core/users/routes.test.js
+++ backend/test/core/users/routes.test.js
@@ -5,15 +5,23 @@ const rootUsersRoute = '/users'
 
 
 describe('Users router test', () => {
-  test('root route POST (create user) ', done => {
-    request(app)
-      .post(rootUsersRoute)
-      .then(({statusCode, body})=>{
-        expect(statusCode).toBe(201)
-        expect(body).toHaveProperty('status', 'user created')
+  const usersFields = {
+    name: 'exampleUserName',
+    password: 'aMagistralPassword',
+    email: 'example@test.com'
+  }
+  describe('Root route test', () => {
+    test('POST (create user) ', done => {
+      request(app)
+        .post(rootUsersRoute)
+        .send(usersFields)
+        .then(({statusCode, body})=>{
+          expect(statusCode).toBe(201)
+          expect(body).toHaveProperty('status', 'user created')
 
-        done()
-      })
-
+          done()
+        })
+    })
   })
+
 })
