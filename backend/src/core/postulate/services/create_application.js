e9276984b08a96ef5352caef2d0c2ee641af205b
EzequielK-source <programacionezequielk@gmail.com>
2022-11-30 09:37:33 -0300

    Add cv_path validations before postulate persist

--- backend/src/core/postulate/services/create_application.js
+++ backend/src/core/postulate/services/create_application.js
@@ -5,6 +5,8 @@ module.exports = async ({ user_id, email,  jobId, message, cv_path}) => {
    * Persit postulate and return they id, email, message and cv
    */
   if(!email || email === '' ) throw new Error('Missing email')
+  // eslint-disable-next-line camelcase
+  if(!cv_path || cv_path === '') throw new Error('Missing cv_path')
   const id = uuidv4()
   await PostulateModel.create({id, email, fk_jobs_id: jobId, message, cv_path})
   return {
